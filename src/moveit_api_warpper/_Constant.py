23027beea6942cfd85cd1dcf4a0db18de2c56a9a
Shuangda <470963660@qq.com>
2019-04-25 12:06:52 +0800

    cpu demo

--- src/moveit_api_warpper/_Constant.py
+++ src/moveit_api_warpper/_Constant.py
@@ -2,7 +2,7 @@ from geometry_msgs.msg import PoseStamped, Pose
 
 memoeryChip_camera_detection= [-0.3131878674030304, 0.49928998947143555, 1.9762904644012451, 0.9798975586891174, 1.3386096954345703, -1.6718480587005615]
 
-cpu_camera_detection= [-0.3131878674030304, 0.49928998947143555, 1.9762904644012451, 0.9798975586891174, 1.3386096954345703, -1.6718480587005615]
+cpu_camera_detection= [0.7852657437324524, 0.6326634883880615, 0.9795230627059937, 3.1773641109466553, -1.5323749780654907, -3.004542350769043]
 
 home = [0.4102410078048706, 0.5366256237030029, 1.2140278816223145, 2.9551632404327393, -1.4722083806991577, -3.4526872634887695]
 
@@ -31,7 +31,7 @@ memoeryChip_insertPose.orientation.w = 0.00454837112943
 cpu_pickPose = Pose()
 cpu_pickPose.position.x = 0.389964938732
 cpu_pickPose.position.y = 0.652318908239
-cpu_pickPose.position.z = 0.474
+cpu_pickPose.position.z = 0.479
 cpu_pickPose.orientation.x = 0.927640427381
 cpu_pickPose.orientation.y = -0.372936390899
 cpu_pickPose.orientation.z = 0.01954549467
@@ -40,7 +40,7 @@ cpu_pickPose.orientation.w = 0.00443390005975
 cpu_insertPose = Pose()
 cpu_insertPose.position.x = 0.623848058274
 cpu_insertPose.position.y = 0.281956277387
-cpu_insertPose.position.z = 0.49
+cpu_insertPose.position.z = 0.499
 cpu_insertPose.orientation.x = -0.370098384544
 cpu_insertPose.orientation.y = -0.928907983007
 cpu_insertPose.orientation.z = -0.0124659623909
